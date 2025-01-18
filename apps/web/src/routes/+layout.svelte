f203b023fcef6105c30ce64b402485c30e368ffe
FredViken <fredrik.mjos@hotmail.com>
2025-01-18 17:29:13 +0100

    style: update color scheme and branding elements

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -7,18 +7,17 @@
 	let isScrolled = $derived(scrollY.current ? scrollY.current > 100 : false);
 </script>
 
-<nav class="fixed left-0 right-0 top-0 z-50 w-full duration-300 {isScrolled ? 'bg-background/75 backdrop-blur-sm' : ''}">
+<nav
+	class="fixed left-0 right-0 top-0 z-50 w-full duration-300 {isScrolled
+		? 'bg-background/75 backdrop-blur-sm'
+		: ''}"
+>
 	<div class="container flex h-16 items-center justify-between">
 		<div class="flex items-center gap-2">
-			<div
-				class="flex size-8 items-center justify-center rounded-sm bg-rose-500 font-bold text-white shadow-inner shadow-white/20"
-			>
-				P
-			</div>
-			<span class="text-xl font-bold">Papir.dev</span>
+			{@render logo()}
+			<span class="text-xl font-bold text-brand">Prismer.io</span>
 		</div>
-		<div class="items-center gap-6 text-muted-foreground hidden sm:flex">
-
+		<div class="hidden items-center gap-6 text-muted-foreground sm:flex">
 			<a href="/">Home</a>
 			<a href="/pricing">Pricing</a>
 			<a href="/docs">Docs</a>
@@ -31,3 +30,18 @@
 	</div>
 </nav>
 {@render children()}
+
+{#snippet logo()}
+	<img src="/logo.svg" alt="Prismer" class="size-8" />
+	<!-- <div class="grid">
+		<div class="relative size-6 rotate-45 rounded-sm drop-shadow-md col-start-1 row-start-1">
+			<div class="bg-gradient-to-b from-brand/50 to-brand/10 absolute inset-0 h-full w-3/4 rounded-[2px]"></div>
+			<div class="bg-gradient-to-l from-brand/50 to-brand/10 absolute bottom-0 h-3/4 w-full rounded-[2px] mix-blend-hard-light"></div>
+		</div>
+		<div class="relative size-6 rotate-45 rounded-sm drop-shadow-md col-start-1 row-start-1">
+			<div class="bg-gradient-to-b from-brand/50 to-brand/10 absolute inset-0 h-full w-3/4 rounded-[2px]"></div>
+			<div class="bg-gradient-to-l from-brand/50 to-brand/10 absolute bottom-0 h-3/4 w-full rounded-[2px] mix-blend-hard-light"></div>
+		</div>
+		
+	</div> -->
+{/snippet}
